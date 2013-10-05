a17d400ae31065711900f45c9cf87c790b48afb0
Brandon Stansbury <brandonrstansbury@gmail.com>
2013-10-05 15:13:38 -0700

    add ctest arena command line project

---
+++ CTestArena/CTestArena/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  CTestArena
+//
+//  Created by Brandon Stansbury on 10/5/13.
+//  Copyright (c) 2013 Monkey Bits. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+

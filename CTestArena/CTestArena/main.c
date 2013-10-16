f631598ef0b4934f154e4ac9809da2741cc221be
Brandon Stansbury <brandonrstansbury@gmail.com>
2013-10-15 18:28:38 -0700

    add testing for working with pointer args

--- CTestArena/CTestArena/main.c
+++ CTestArena/CTestArena/main.c
@@ -57,11 +57,56 @@ void reassign_struct_member(void)
     printf("Parent after value reassignment: %d --> %d\n", parent.value, parent.child->value);
 }
 
+void change_pointer(int *pointer)
+{
+    printf("CP: Pointer is %p\n", pointer);
+    printf("CP: Pointer points to %d\n", *pointer);
+    pointer = malloc(sizeof(int));
+    *pointer = 8;
+    printf("CP: Pointer is %p\n", pointer);
+    printf("CP: Pointer points to %d\n", *pointer);
+    free(pointer);
+}
+void change_pointedto(int *pointer)
+{
+    printf("CPT: Pointer is %p\n", pointer);
+    printf("CPT: Pointer points to %d\n", *pointer);
+    int *temp = malloc(sizeof(int));
+    *temp = 12;
+    printf("CPT: Temp is %p\n", temp);
+    printf("CPT: Temp points to %d\n", *temp);
+    *pointer = *temp;
+    printf("CPT: Pointer is %p\n", pointer);
+    printf("CPT: Pointer points to %d\n", *pointer);
+    free(temp);
+}
+void modify_pointer_arg(void)
+{
+    printf("-- modify_pointer_arg --\n");
+    int v = 4;
+    int *foo = &v;
+    printf("foo is %p\n", foo);
+    printf("foo points to %d\n", *foo);
+    printf("v is %d\n", v);
+    change_pointer(foo);
+    printf("foo is %p\n", foo);
+    printf("foo points to %d\n", *foo);
+    printf("v is %d\n", v);
+    change_pointedto(foo);
+    printf("foo is %p\n", foo);
+    printf("foo points to %d\n", *foo);
+    printf("v is %d\n", v);
+    // can't do this without a ** parameter
+//    foo = NULL;
+//    change_pointedto(foo);
+}
+
 int main(int argc, const char *argv[])
 {
     binarytrees();
     compare_sizeof();
     reassign_struct_member();
+    modify_pointer_arg();
     
     return EXIT_SUCCESS;
 }

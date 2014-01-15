c59b7a3b718fde193a5e62bd269189f4dd2843a3
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-01-14 20:50:24 -0800

    add binary tree test file

---
+++ CTestArena/CTestArenaTests/Modules/BinaryTreeTests.m
@@ -0,0 +1,34 @@
+//
+//  BinaryTreeTests.m
+//  CTestArena
+//
+//  Created by Brandon Stansbury on 1/14/14.
+//  Copyright (c) 2014 Monkey Bits. All rights reserved.
+//
+
+#include "binary_tree.h"
+
+@interface BinaryTreeTests : XCTestCase
+
+@end
+
+@implementation BinaryTreeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

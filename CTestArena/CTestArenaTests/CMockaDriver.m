7b1f37e6ceca41565569a9f9203b6162b73932a4
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-03-30 19:51:06 -0700

    wire tests up to cmocka driver so xcode can run them on cmd+u

--- CTestArena/CTestArenaTests/CMockaDriver.m
+++ CTestArena/CTestArenaTests/CMockaDriver.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Monkey Bits. All rights reserved.
 //
 
-#import <cmocka/cmocka.h>
+int test_main(void);
 
 @interface CMockaDriver : XCTestCase
 
@@ -14,21 +14,9 @@
 
 @implementation CMockaDriver
 
-- (void)setUp
+- (void)test_main
 {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    XCTAssertEqual(0, test_main());
 }
 
 @end

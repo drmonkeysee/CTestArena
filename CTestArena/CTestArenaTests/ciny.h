97c360e0eaf8a0c0ad22f38b4a6641b8b86ab3f3
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-04-05 16:29:52 -0700

    add ciny header with todo

---
+++ CTestArena/CTestArenaTests/ciny.h
@@ -0,0 +1,31 @@
+//
+//  ciny.h
+//  CTestArena
+//
+//  Created by Brandon Stansbury on 4/5/14.
+//  Copyright (c) 2014 Monkey Bits. All rights reserved.
+//
+
+#ifndef CTestArena_ciny_h
+#define CTestArena_ciny_h
+
+/* TODO
+ - basic assertions
+    - assert fail
+    - assert true
+    - assert false
+    - assert null
+    - assert not null
+    - assert equal
+    - assert not equal
+    - assert equal within epsilon
+    - assert not equal within epsilon
+ - setup / teardown
+ - define suite list
+ - stdout/stderr output with color
+ - string comparison
+ - struct comparison
+ - memory comparison
+ */
+
+#endif

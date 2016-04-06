4fc107812e2511b3498d4de3e59c1dc5aa0f25a4
Brandon Stansbury <brandonrstansbury@gmail.com>
2016-04-05 19:41:38 -0700

    more aliasing examples with restrict

--- CTestArena/CTestArena/aliasing.c
+++ CTestArena/CTestArena/aliasing.c
@@ -8,14 +8,39 @@
 
 #include <stddef.h>
 
-void one_write(int * restrict a, const int * restrict b, const int * restrict c, size_t n)
+#define RESTRICT_KW restrict
+
+void one_write(int * RESTRICT_KW a, const int * RESTRICT_KW b, const int * RESTRICT_KW c, size_t n)
 {
     for (size_t i = 0; i < n; ++i) {
         a[i] = b[i] + c[i];
     }
 }
 
-void single_update(int * restrict a, int * restrict b, int * restrict val)
+// restrict omits a movl
+void sametype_aliasing(int * RESTRICT_KW a, int * RESTRICT_KW b, int * RESTRICT_KW val)
+{
+    *a += *val;
+    *b += *val;
+}
+
+// restrict has no effect
+void mixedtype_aliasing(int * RESTRICT_KW a, int * RESTRICT_KW b, short * RESTRICT_KW val)
+{
+    *a += *val;
+    *b += *val;
+}
+
+// restrict omits a movl
+void void_aliasing(int * RESTRICT_KW a, int * RESTRICT_KW b, void * RESTRICT_KW val)
+{
+    int *int_val = val;
+    *a += *int_val;
+    *b += *int_val;
+}
+
+// restrict omits a movsbl
+void char_aliasing(int * RESTRICT_KW a, int * RESTRICT_KW b, char * RESTRICT_KW val)
 {
     *a += *val;
     *b += *val;
